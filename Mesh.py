b8c02dde59a3cd26cdf3398a20312527cf30853b
markansn <contact@markanson.co>
2021-02-27 00:58:18 +0000

    Serialization (#2)
    * added serialization
    
    * removed .idea
    
    * fixed git error

--- Mesh.py
+++ Mesh.py
@@ -1,6 +1,31 @@
+import json
+
+
 class Mesh:
     def __init__(self, verts, tInd):
         self.verts = verts
         self.tInd = tInd
+
     def copy(self):
         return Mesh(self.verts.copy(), self.tInd.copy())
+
+    def print_data(self):  # dump Mesh data to stdout
+        print("------ VERTS ------")
+        print(self.verts)
+        print("------ tIND ------")
+        print(self.tInd)
+
+    def save(self, file):  # save data to specified file
+        data = {
+            "verts": self.verts,
+            "tInd": self.tInd
+        }
+
+        with open(file, 'w') as f:
+            json.dump(data, f)
+
+    def load(self, file):  # load data to specified file (will overwrite existing data in object)
+        with open(file) as f:
+            data = json.load(f)
+        self.verts = data["verts"]
+        self.tInd = data["tInd"]
