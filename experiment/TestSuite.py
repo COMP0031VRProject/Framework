41675eaafa61bc4dbc8cf052f9613d3a9ea15b99
Yangtao Ge <yangtao.ge.17@ucl.ac.uk>
2021-03-14 01:18:44 +0000

    add sequence generator

--- experiment/TestSuite.py
+++ experiment/TestSuite.py
@@ -1,5 +1,8 @@
 import json
+import math
 from Trial import *
+from itertools import permutations
+from jsonUtils import *
 
 class TestSuite:
     def __init__(self, suite_id, flag_num, distance):
@@ -8,9 +11,33 @@ class TestSuite:
         self.distance = distance
         self.trial_id = 0
         self.trials = []
+
+    def distance_between(self, x, y):
+        x_val = x.value
+        y_val = y.value
+        rad_unit = math.pi / 3
+        dist = math.sqrt(2 - 2*math.cos((x_val - y_val)*rad_unit))
+
+        return round(dist, 2)
     
-    def generate():
-        pass
+    def generate(self):
+        combs = list(permutations(Flag, self.flag_num))
+
+        for comb in combs:
+            dist = 1
+            sequence = []
+            for flag in comb:
+                sequence.append(flag.name)
+            for i in range(self.flag_num - 1):
+                curr = comb[i]
+                next = comb[i+1]
+                dist += self.distance_between(curr, next)
+
+            if dist == self.distance:
+                self.trial_id += 1
+                trial = Trial(self.trial_id, sequence)
+                self.trials.append(trial.__dict__)
+
 
     def save(self, file):
         data = {
